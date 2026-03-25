e51a7580545b5b5f7151a03c04db348709072873
Noor Sohail <20noor.sohail@gmail.com>
2026-03-24 21:35:13 -0400

    Scripts for first few lessons written

---
+++ scripts/06_banksy.R
@@ -0,0 +1,161 @@
+############################################################
+# Spatially Derived Clusters with BANKSY
+# Visium HD spatial transcriptomics workshop
+# Author: Harvard Chan Bioinformatics Core
+############################################################
+
+library(Seurat)
+# remotes::install_github("prabhakarlab/Banksy")
+library(Banksy)
+library(SeuratWrappers)
+library(tidyverse)
+library(qs)
+
+############################################################
+# Load processed Seurat object
+############################################################
+
+seurat_processed <- qread("")
+seurat_processed
+
+############################################################
+# Prepare coordinates for BANKSY (x, y per bin)
+############################################################
+
+seurat_processed$cell <- rownames(seurat_processed@meta.data)
+
+# CRC and NAT sample coordinates
+coords_crc <- GetTissueCoordinates(seurat_processed, image = "P5CRC.008um")
+coords_nat <- GetTissueCoordinates(seurat_processed, image = "P5NAT.008um")
+coords <- rbind(coords_crc, coords_nat)
+
+# Join coords into metadata
+seurat_processed@meta.data <- left_join(
+  seurat_processed@meta.data,
+  coords,
+  by = "cell"
+)
+rownames(seurat_processed@meta.data) <- seurat_processed$cell
+
+############################################################
+# Run BANKSY (creates BANKSY assay)
+############################################################
+
+# Example full run (can take a while):
+# seurat_banksy <- RunBanksy(
+#   seurat_processed,
+#   lambda       = 0.6,
+#   verbose      = TRUE,
+#   assay        = "Spatial.008um",
+#   slot         = "data",
+#   k_geom       = 15,
+#   dimx         = "x",
+#   dimy         = "y",
+#   group        = "orig.ident",
+#   split.scale  = TRUE
+# )
+
+# For convenience, load precomputed object instead:
+# saveRDS(seurat_banksy, "data/seurat_banksy_1.RDS")
+seurat_banksy <- readRDS("data/seurat_banksy_1.RDS")
+
+# BANKSY is now the active assay
+seurat_banksy
+
+############################################################
+# Inspect BANKSY features (original + neighborhood-weighted)
+############################################################
+
+# First few features (original genes)
+Features(seurat_banksy) %>% head()
+
+# Last few features (lambda-scaled neighborhood-weighted, e.g. *.m0)
+Features(seurat_banksy) %>% tail()
+
+############################################################
+# PCA + kNN + clustering on BANKSY matrix
+############################################################
+
+# Example full pipeline:
+# options(future.globals.maxSize = 200000000000)
+#
+# seurat_banksy <- RunPCA(
+#   seurat_banksy,
+#   assay          = "BANKSY",
+#   reduction.name = "pca.banksy",
+#   features       = rownames(seurat_banksy),
+#   npcs           = 30
+# )
+#
+# seurat_banksy <- FindNeighbors(
+#   seurat_banksy,
+#   reduction = "pca.banksy",
+#   dims      = 1:30
+# )
+#
+# seurat_banksy <- FindClusters(
+#   seurat_banksy,
+#   cluster.name = "banksy_cluster",
+#   resolution   = 0.8
+# )
+
+# After this, the important outputs are:
+# - banksy_cluster in @meta.data
+# - pca.banksy in @reductions
+
+# Once done, we can drop the BANKSY assay to save memory:
+# DefaultAssay(seurat_banksy) <- "Spatial.008um"
+# seurat_banksy[["BANKSY"]] <- NULL
+
+# For convenience, load post-processing object:
+# saveRDS(seurat_banksy, "data/seurat_banksy_2.RDS")
+seurat_banksy <- readRDS("data/seurat_banksy_2.RDS")
+
+############################################################
+# Visualize BANKSY clusters on tissue
+############################################################
+
+SpatialDimPlot(
+  seurat_banksy,
+  group.by      = "banksy_cluster",
+  pt.size.factor = 13,
+  image.alpha    = 0
+)
+
+############################################################
+# Compare BANKSY clusters vs RNA-based (projected) clusters
+############################################################
+
+# Spatial overlay of both clusterings
+SpatialDimPlot(
+  seurat_banksy,
+  group.by       = c("banksy_cluster", "seurat_cluster.projected"),
+  pt.size.factor = 13,
+  image.alpha    = 0
+)
+
+# UMAP comparison of both clusterings
+DimPlot(
+  seurat_banksy,
+  group.by = c("banksy_cluster", "seurat_cluster.projected")
+)
+
+############################################################
+# Batch composition per cluster: RNA-based vs BANKSY
+############################################################
+
+# Proportion of cells in each RNA-based projected cluster by sample
+ggplot(seurat_banksy@meta.data) +
+  geom_bar(
+    aes(x = seurat_cluster.projected, fill = orig.ident),
+    position = position_fill()
+  ) +
+  theme_classic()
+
+# Proportion of cells in each BANKSY cluster by sample
+ggplot(seurat_banksy@meta.data) +
+  geom_bar(
+    aes(x = banksy_cluster, fill = orig.ident),
+    position = position_fill()
+  ) +
+  theme_classic()
---
+++ scripts/data/download_data.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# ############## MOUSE BRAIN
+# # Mouse brain dataset from 10x
+# # Fresh Frozen
+# # https://www.10xgenomics.com/datasets/visium-hd-cytassist-gene-expression-mouse-brain-fresh-frozen
+# mkdir -p fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_web_summary.html --output-dir fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_cloupe_008um.cloupe --output-dir fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_feature_slice.h5 --output-dir fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_metrics_summary.csv --output-dir fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_molecule_info.h5 --output-dir fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_spatial.tar.gz --output-dir fresh_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fresh_Frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_binned_outputs.tar.gz --output-dir fresh_frozen
+# 
+# # Unzip bin folder
+# tar -xvf fresh_frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_binned_outputs.tar.gz --directory fresh_frozen
+# tar -xvf fresh_frozen/Visium_HD_Mouse_Brain_Fresh_Frozen_spatial.tar.gz --directory fresh_frozen
+# 
+# # Fixed Frozen
+# # https://www.10xgenomics.com/datasets/visium-hd-cytassist-gene-expression-mouse-brain-fixed-frozen
+# mkdir -p fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_web_summary.html --output-dir fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_cloupe_008um.cloupe --output-dir fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_feature_slice.h5 --output-dir fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_metrics_summary.csv --output-dir fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_molecule_info.h5 --output-dir fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_spatial.tar.gz --output-dir fixed_frozen
+# curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.1.1/Visium_HD_Mouse_Brain_Fixed_Frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_binned_outputs.tar.gz --output-dir fixed_frozen
+# 
+# # Unzip bin folder
+# tar -xvf fixed_frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_binned_outputs.tar.gz --directory fixed_frozen
+# tar -xvf fixed_frozen/Visium_HD_Mouse_Brain_Fixed_Frozen_spatial.tar.gz --directory fixed_frozen
+
+
+################ HUMAN CRC
+# CRC dataset from 10x
+# https://www.10xgenomics.com/platforms/visium/product-family/dataset-human-crc
+
+mkdir -p P5CRC
+mkdir -p P5NAT
+
+# Sample P5 CRC
+# Output Files
+cd P5CRC
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Cancer_P5/Visium_HD_Human_Colon_Cancer_P5_binned_outputs.tar.gz
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Cancer_P5/Visium_HD_Human_Colon_Cancer_P5_spatial.tar.gz
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Cancer_P5/Visium_HD_Human_Colon_Cancer_P5_molecule_info.h5
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Cancer_P5/Visium_HD_Human_Colon_Cancer_P5_cloupe_008um.cloupe
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Cancer_P5/Visium_HD_Human_Colon_Cancer_P5_feature_slice.h5
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Cancer_P5/Visium_HD_Human_Colon_Cancer_P5_metrics_summary.csv
+cd ../
+
+# Sample P5 NAT
+# Output Files
+cd P5NAT
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Normal_P5/Visium_HD_Human_Colon_Normal_P5_binned_outputs.tar.gz
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Normal_P5/Visium_HD_Human_Colon_Normal_P5_spatial.tar.gz
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Normal_P5/Visium_HD_Human_Colon_Normal_P5_molecule_info.h5
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Normal_P5/Visium_HD_Human_Colon_Normal_P5_cloupe_008um.cloupe
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Normal_P5/Visium_HD_Human_Colon_Normal_P5_feature_slice.h5
+curl -O https://cf.10xgenomics.com/samples/spatial-exp/3.0.0/Visium_HD_Human_Colon_Normal_P5/Visium_HD_Human_Colon_Normal_P5_metrics_summary.csv
+cd ../
+
+# Unzip CRC files
+tar -xvf P5NAT/Visium_HD_Human_Colon_Normal_P5_binned_outputs.tar.gz -C P5NAT
+tar -xvf P5NAT/Visium_HD_Human_Colon_Normal_P5_spatial.tar.gz -C P5NAT
+tar -xvf P5CRC/Visium_HD_Human_Colon_Cancer_P5_binned_outputs.tar.gz -C P5CRC
+tar -xvf P5CRC/Visium_HD_Human_Colon_Cancer_P5_spatial.tar.gz -C P5CRC
+
+# Published metadata for cells (barcodes are in completely different format)
+wget https://github.com/10XGenomics/HumanColonCancer_VisiumHD/raw/refs/heads/main/MetaData/SingleCell_MetaData.csv.gz
+gunzip SingleCell_MetaData.csv.gz
+
+# Deconvolution results
+wget https://github.com/10XGenomics/HumanColonCancer_VisiumHD/raw/refs/heads/main/MetaData/DeconvolutionResults_P5CRC.csv.gz
+gunzip DeconvolutionResults_P5CRC.csv.gz
+wget https://github.com/10XGenomics/HumanColonCancer_VisiumHD/raw/refs/heads/main/MetaData/P5CRC_Metadata.parquet
+
+
+mkdir flex_reference
+mv DeconvolutionResults_P5CRC.csv flex_reference/
+mv SingleCell_MetaData.csv flex_reference/
+mv P5CRC_Metadata.parquet flex_reference/
